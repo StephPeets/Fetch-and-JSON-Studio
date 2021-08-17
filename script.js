60d982298fa9e5a5dc2544f028ad6aa405972b11
Steph <84749465+StephPeets@users.noreply.github.com>
2021-08-16 23:03:17 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// TODO: add code here
+
+window.addEventListener('load', function () {
+  const container = document.getElementById('container');
+  fetch('https://handlers.education.launchcode.org/static/astronauts.json')
+    .then((response) => response.json())
+    .then((data) => {
+      data.sort(function (a, b) {
+        return b.hoursInSpace - a.hoursInSpace;
+      });
+      for (let index = 0; index < data.length; index++) {
+        container.innerHTML += `
+          <div class="astronaut">
+          <div class="bio">
+          <h3>${data[index].firstName} ${data[index].lastName}</h3>
+            <ul>
+              <li>Hours in space: ${data[index].hoursInSpace}</li>
+              <li>Active: ${data[index].active}</li>
+              <li>Skills: ${data[index].skills.join(', ')}</li>
+            </ul>
+          </div>
+          <img class="avatar" src="${data[index].picture}">`;
+      }
+    });
+});
